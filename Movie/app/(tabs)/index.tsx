24346d80ff76982378a5058f2696b5d0ab629c89
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-07-21 11:34:05 -0400

    feat: add search bar to home screen

--- Movie/app/(tabs)/index.tsx
+++ Movie/app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, Text, View } from "react-native";
+import SearchBar from "../components/searchBar";
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 bg-primary"
-    >
+    <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute top-0 w-full" />
-      <ScrollView className="flex-1" showsHorizontalScrollIndicator={false} contentContainerStyle={{minHeight:"100%", paddingBottom: 10}}>
+      <ScrollView
+        className="flex-1"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
         <Image source={icons.logo} className="size-12 mt-20 mb-5 mx-auto" />
+        <View className="mt-5">
+          <SearchBar />
+        </View>
       </ScrollView>
     </View>
   );

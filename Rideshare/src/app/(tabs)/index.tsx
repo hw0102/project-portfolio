e9d7772e36b87422d0ed06872d327bb74bf2de74
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-07-27 13:17:34 -0400

    chore: update asset used in the project

---
+++ Rideshare/src/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl text-blue-600">Welcome to Nativewind!</Text>
+    </SafeAreaView>
+  );
+}
---
+++ Rideshare/src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {/*<Stack.Screen name="Home" />*/}
+    </Stack>
+  );
+};
+
+export default RootLayout;

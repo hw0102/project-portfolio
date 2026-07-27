ae6a128dc3e1600c41e6b7036500ae2c1b1b7bc5
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-07-27 12:38:31 -0400

    fix: resolve outdated cache preventing native wind from working

--- Rideshare/tailwind.config.js
+++ Rideshare/tailwind.config.js
@@ -1,13 +1,10 @@
-const { colors } = require("./constants/theme");
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors,
-    },
+    extend: {},
   },
   plugins: [],
-};
+}

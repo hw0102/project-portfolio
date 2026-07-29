6d33cd0d38e05e6f74f08bf23099d5d15de272c1
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-07-29 11:09:02 -0400

    setup: route setup

---
+++ Rideshare/src/app/(root)/(auth)/welcome.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+  return (
+    <SafeAreaView>
+      <Text> Home </Text>
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;

3743341d77d0667df86e6530c1b7922485360afa
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-07-29 10:17:06 -0400

    setup: add custom text in RN and tailwind (also custom colors)

--- Rideshare/src/app/_layout.tsx
+++ Rideshare/src/app/_layout.tsx
@@ -1,12 +1,31 @@
-import { Stack } from "expo-router";
 import "../global.css";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
-const RootLayout = () => {
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Jakarta-Bold": require("../../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "Jakarta-Regular": require("../../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       {/*<Stack.Screen name="index" />*/}
     </Stack>
   );
-};
-
-export default RootLayout;
+}

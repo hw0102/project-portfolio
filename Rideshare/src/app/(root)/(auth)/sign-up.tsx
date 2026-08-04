94639a373c7c0211f3db3db470fd5a6bfdd56170
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-08-04 11:56:29 -0400

    chore: convenience import for images
    add sign-up page backdrop

--- Rideshare/src/app/(root)/(auth)/sign-up.tsx
+++ Rideshare/src/app/(root)/(auth)/sign-up.tsx
@@ -1,11 +1,19 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import { Image, ScrollView, Text } from "react-native";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text> This is the sign up page </Text>
-    </SafeAreaView>
+    <ScrollView contentContainerClassName="grow" className="bg-white">
+      <Image
+        source={images.signUpCar}
+        className="w-full h-[250px] z-0"
+        resizeMode="cover"
+      />
+      <Text className="font-JakartaSemiBold absolute top-[200px] left-3 text-2xl">
+        {" "}
+        Create Your Account{" "}
+      </Text>
+    </ScrollView>
   );
 };
 

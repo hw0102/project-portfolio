e6698ffee658fb9b98c9f1d34f8f86103488fb4c
hw0102 <105983572+hw0102@users.noreply.github.com>
2026-08-07 07:42:14 -0400

    feat: native tab navigation with sf symbols

--- Rideshare/src/app/(root)/(tabs)/_layout.tsx
+++ Rideshare/src/app/(root)/(tabs)/_layout.tsx
@@ -1,7 +1,27 @@
-import { Stack } from "expo-router";
+import { NativeTabs } from "expo-router/build/native-tabs";
 
 const Layout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="home">
+        <NativeTabs.Trigger.Label>home</NativeTabs.Trigger.Label>
+        {/*<NativeTabs.Trigger.Icon src={icons.home} />*/}
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="chat">
+        <NativeTabs.Trigger.Label>chat</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="bubble" md="chat" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="profile">
+        <NativeTabs.Trigger.Label>Profile</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="person.crop.circle" md="account_circle" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="rides">
+        <NativeTabs.Trigger.Label>Rides</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="car" md="airport_shuttle" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 };
 
 export default Layout;
